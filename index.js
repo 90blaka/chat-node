0e30a6a3010778e695421c4b930a8f57296ceb9c
90blaka <111440191+90blaka@users.noreply.github.com>
2022-08-17 09:38:50 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+// DB Config
+require('./database/config').dbConnetion();
+
+//App Express // este comentario se elimina
+const app = express();
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+//Node server
+const server = require("http").createServer(app);
+module.exports.io = require("socket.io")(server);
+require('./sockets/socket');
+
+//Public path
+const publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
+
+// Mis Rutas
+app.use( '/api/login', require('./routes/auth') );
+app.use( '/api/usuarios', require('./routes/usuarios') );
+app.use( '/api/mensajes', require('./routes/mensajes') );
+
+server.listen(process.env.PORT, (err) => {
+    if (err) throw new Error(err);
+
+    console.log(`Servidor 'corriendo' en el puerto 3000`);
+});
